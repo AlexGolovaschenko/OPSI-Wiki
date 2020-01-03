51239fe64137387cb59109c42fd0a70a32887670
Aleksandr <golovasanya@mail.ru>
2020-01-03 18:12:54 +0200

    added SectionInlineFormSet

--- pages/forms.py
+++ pages/forms.py
@@ -4,15 +4,25 @@ from pages.models import Page, Section
 
 
 class PageCreateForm (forms.ModelForm):
-    class Meta:
-        model = Page
-        fields = ['category', 'topic', 'name', 'short_description']
+	class Meta:
+		model = Page
+		fields = ['category', 'topic', 'name', 'short_description']
  
 
 class SectionCreateForm (forms.ModelForm):
-    headline = forms.CharField(max_length=200, required=False, label='Оглавление раздела')
-    content = forms.CharField(widget=CKEditorWidget(), required=False, label='Раздел')
+	headline = forms.CharField(max_length=200, required=False, label='Оглавление раздела')
+	content = forms.CharField(widget=CKEditorWidget(), required=False, label='Раздел')
 
-    class Meta:
-        model = Section
-        fields = ['headline', 'content']
+	class Meta:
+		model = Section
+		fields = ['headline', 'content']
+
+
+def getSectionInlineFormSet (can_delete=False):
+	return forms.inlineformset_factory(
+		Page,
+		Section, 
+		form=SectionCreateForm, 
+		can_delete=can_delete,
+		extra = 1)
+
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, get_list_or_404
 from pages.models import Page, Category, Section
-from pages.forms import PageCreateForm, SectionCreateForm
+from pages.forms import PageCreateForm, SectionCreateForm, getSectionInlineFormSet
 
 # def pages_list(request):
 # 	return render(request, 'pages/pages_list.html')
@@ -49,67 +49,58 @@ def page_detail(request, id):
 	return render(request, 'pages/page_detail.html', context)
 
 
+
 def create_page(request):
 	if request.method == "POST":
 		page_form = PageCreateForm(request.POST)
-		section_form = SectionCreateForm(request.POST)
-
-		if page_form.is_valid() and section_form.is_valid():
+		
+		if page_form.is_valid() :
 			page = page_form.save()
-			section = section_form.save(commit=False)
-			if (section.headline != '') or (section.content != ''): 
-				section.page = page
-				section.save()
+
+			section_forms = getSectionInlineFormSet()(request.POST, instance = page)
+			if section_forms.is_valid():
+				section_forms.save()
+			else:
+				messages.warning(request, f'Возникла ошибка при создании раздела!')
+
 			messages.success(request, f'Страница успешно создана!')
-			return redirect('pages:detail', str(page.id))
+			return redirect('pages:detail', page.id)
 		else:
 			messages.warning(request, f'Возникла ошибка при создании страницы!')
 			return redirect('home')
 	else:
 		page_form = PageCreateForm()
-		section_form = SectionCreateForm()
-
-
+		section_forms = getSectionInlineFormSet()()
+		
 	context = {
 		'page_form': page_form,
-		'section_form' : section_form,
+		'section_forms' : section_forms,
 	}
 
-	return render(request, 'pages/create_page.html', context)
+	return render(request, 'pages/update_page.html', context)
 
 
 def update_page(request, id):
-	extra_sections = 1
 	page = get_object_or_404(Page, pk=id)
-	try:
-		sections = get_list_or_404(Section, page=page)
-		section = sections[0]
-	except:
-		sections = get_list_or_404(Section)
-		section = sections[0]
 
 	if request.method == "POST":
 		page_form = PageCreateForm(request.POST, instance = page)
-		section_form = SectionCreateForm(request.POST, instance = section)
+		section_forms = getSectionInlineFormSet(can_delete=True)(request.POST, instance = page)
 
-		if page_form.is_valid() and section_form.is_valid():
-			page = page_form.save()
-			section = section_form.save(commit=False)
-			if (section.headline != '') or (section.content != ''): 
-				section.page = page
-				section.save()
+		if page_form.is_valid() and section_forms.is_valid():
+			page_form.save()
+			section_forms.save()
 			messages.success(request, f'Страница успешно изменена!')
 		else:
 			messages.warning(request, f'Возникла ошибка при редактировании страницы!')
 		return redirect('pages:detail', str(id))
 	else:
 		page_form = PageCreateForm(instance = page)
-		section_form = SectionCreateForm(instance = section)
-
-
+		section_forms = getSectionInlineFormSet(can_delete=True)(instance=page)
+		
 	context = {
 		'page_form': page_form,
-		'section_form' : section_form,
+		'section_forms' : section_forms,
 	}
 
 	return render(request, 'pages/update_page.html', context)
